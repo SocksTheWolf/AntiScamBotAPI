534b6442cb8d3c4b6f985174b064da01017b6e31
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2024-08-31 14:43:22 -0700

    Create DatabaseDriver.py

---
+++ DatabaseDriver.py
@@ -0,0 +1,61 @@
+from DatabaseSchema import Ban
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, select, URL, desc, func
+from sqlalchemy.orm import Session
+
+load_dotenv()
+
+# Mostly based off of the ScamGuard Database Option
+class DatabaseDriver():
+	Database = None
+
+	### Initialization/Teardown ###
+	def __init__(self, *args, **kwargs):
+		self.Open()
+
+	def __del__(self):
+		self.Close()
+
+	def Open(self):
+		self.Close()
+
+		database_url = URL.create(
+			'sqlite',
+			username='',
+			password='',
+			host='',
+			database=Config.GetDBFile(),
+		)
+		self.Database = Session(create_engine(database_url))
+
+	def Close(self):
+		if (self.IsConnected()):
+			self.Database.get_bind().dispose()
+			self.Database = None
+		
+	def IsConnected(self) -> bool:
+		if (self.Database is not None):
+			return True
+		return False
+
+	@staticmethod
+	def GetDatabaseFile() -> str:
+		return os.getenv("DATABASE_FILE")
+
+	### Lookup Data ###
+	def DoesBanExist(self, TargetId:int) -> bool:
+		stmt = select(Ban).where(Ban.discord_user_id==TargetId)
+		result = self.Database.scalars(stmt).first()
+
+		if (result is None):
+			return False
+
+		return True
+
+	def GetBanInfo(self, TargetId:int) -> Ban|None:
+		stmt = select(Ban).where(Ban.discord_user_id==TargetId)
+		return self.Database.scalars(stmt).first()
+
+	def GetNumBans(self) -> int:
+		stmt = select(func.count()).select_from(Ban)
+		return self.Database.scalars(stmt).first()
