14230978b68b3257a1d2531509d19f84e57d3f0e
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2024-08-31 15:15:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ db = DatabaseDriver()
 
 @app.get("/")
 def main():
-  return {"result": False}
+  return {"msg": "There is no war in ba sing sei"}
   
 @app.get("/check/{user_id}")
 def check_ban(user_id: int):
   IsDataGood:bool = (user_id >= 1)
-  return {"banned": db.DoesBanExist(user_id), "result": IsDataGood}
+  return {"banned": db.DoesBanExist(user_id), "valid": IsDataGood}
 
 @app.get("/ban/{user_id}")
 def get_ban_info(user_id: int):
    BanInfo:Ban|None = db.GetBanInfo(user_id)
    if BanInfo is None:
-     return {"result": False}
+     return {"banned": False, "user_id": user_id}
      
-   return {"result": True, "user_id": BanInfo.discord_user_id, "banned_by": BanInfo.assigner_discord_user_name, "banned_on": BanInfo.created_at}
+   return {"banned": True, "user_id": BanInfo.discord_user_id, "banned_by": BanInfo.assigner_discord_user_name, "banned_on": BanInfo.created_at}
 
 @app.get("/bans")
 def get_ban_stats():
-   return {"result": True, "count": db.GetNumBans()}
+   return {"count": db.GetNumBans()}
