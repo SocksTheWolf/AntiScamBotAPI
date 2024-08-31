7ea27d32dabd00f68a0bb82c7c8e2b36c7f13a6d
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2024-08-31 14:56:10 -0700

    Create run.sh

---
+++ .runtime/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Remember to run this with nohup!
+# nohup .runtime/run.sh &>/dev/null &
+pwd | grep -q .runtime
+if [ "$?" -eq "0" ]; then
+    cd ..
+fi
+python3 -m venv .venv
+source .venv/bin/activate
+python3 ./main.py
