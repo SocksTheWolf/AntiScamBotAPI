26d7fe9c23b093bd4c6a551631269fe2956f29e9
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2024-08-31 14:43:37 -0700

    Create DatabaseSchema.py

---
+++ DatabaseSchema.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, DateTime, String
+from sqlalchemy.sql import func
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Ban(Base):
+    __tablename__ = "bans"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    discord_user_id = Column(String(32), unique=True, nullable=False)
+    assigner_discord_user_id = Column(String(32), nullable=False)
+    assigner_discord_user_name = Column(String(32), nullable=False)
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), server_default=func.now(), onupdate=func.now())
